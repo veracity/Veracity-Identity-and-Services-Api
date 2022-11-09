713e9deaef19272ba96402d457605d3418da169b
Erdem Özkan <erdem.oezkan@dnv.com>
2022-11-09 14:08:23 +0100

    .net5 project folder name fixed, 6 and 7 draft projects created

---
+++ src/Samples/WebApplicationNet5/Startup.cs
@@ -0,0 +1,114 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Options;
+using netCore5.Api;
+using Stardust.Interstellar.Rest.Client;
+using Stardust.Interstellar.Rest.Service;
+using Stardust.Particles;
+using Veracity.Common.Authentication;
+using Veracity.Common.OAuth.Providers;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env)
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
+                .AddAzureKeyVault("https://veracitydevdaydemo.vault.azure.net/", new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback)), new DefaultKeyVaultSecretManager())
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Stardust.Interstellar.Rest.Service.ServiceFactory.ThrowOnException = true;
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddVeracity(Configuration)
+                .AddScoped(s => s.GetService<IHttpContextAccessor>().HttpContext.User)
+                .AddSingleton(ConstructDistributedCache)
+                .AddVeracityServices(ConfigurationManagerHelper.GetValueOnKey("myApiV3Url"), services => services.AddScoped(s => s.CreateRestClient<IWtfClient>("https://localhost:44344/")))
+
+                .AddAuthentication(sharedOptions =>
+                {
+                    sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                })
+
+                .AddVeracityAuthentication(Configuration, isMfaRequiredOptions: (httpContext, authenticationProperties) =>
+                {
+                    //do custom logic there
+                    return true;
+                })
+                .AddCookie();
+
+            services.AddMvc(options => options.EnableEndpointRouting = false)
+                .AddAsController<ITestService, TestApi>()
+                .AddVeracityApiProxies(ConfigurationManagerHelper.GetValueOnKey("myApiV3Url"))
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        private IDistributedCache ConstructDistributedCache(IServiceProvider s)
+        {
+            return new MemoryDistributedCache(new OptionsWrapper<MemoryDistributedCacheOptions>(new MemoryDistributedCacheOptions()));
+            return s.GetService<IDistributedCache>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            //app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseVeracity();
+            app.UseRouting().UseAuthorization().UseEndpoints(r =>
+            {
+                r.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                r.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ src/Samples/WebApplicationNet6/Api/TestApi.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Stardust.Interstellar.Rest.Annotations;
+using Stardust.Interstellar.Rest.Annotations.Rest.Extensions;
+using Stardust.Interstellar.Rest.Extensions;
+using Stardust.Interstellar.Rest.Service;
+using Stardust.Particles;
+using Veracity.Common.Authentication;
+using Veracity.Services.Api.Extensions;
+
+namespace netCore5.Api
+{
+    [Api("api")]
+    public interface ITestService:IServiceExtensions
+    {
+        [Get("")]
+        Task<string> Get();
+    }
+
+    [Api("api")]
+    [MRRTAuth]
+    [CircuitBreaker(1000, 2)]
+    [SupportCode]
+    [ErrorHandler(typeof(ExceptionWrapper))]
+    [AuthorizeWrapper]
+    [ServiceInformation]
+    [AccessControllGate(AccessControllTypes.User, RoleTypes.IsValidUser)]
+    public interface IWtfClient
+    {
+        [Get("")]
+        Task<string> GetTheThingAsync();
+    }
+
+    public class MRRTAuthAttribute : AuthenticationInspectorAttributeBase
+    {
+        public override IAuthenticationHandler GetHandler(IServiceProvider provider)
+        {
+            return new AdditionalScopeHandler(provider);
+        }
+
+        
+    }
+
+    public class AdditionalScopeHandler: IAuthenticationHandler
+    {
+        private readonly TokenProviderConfiguration _configuration;
+        private readonly IServiceProvider _appApplicationServices;
+        public static string Authority(TokenProviderConfiguration configuration) => $"https://login.microsoftonline.com/tfp/{configuration.TenantId}/{configuration.Policy}/v2.0/.well-known/openid-configuration";
+        public AdditionalScopeHandler() : this(new TokenProviderConfiguration())
+        {
+        }
+
+        public AdditionalScopeHandler(IServiceProvider appApplicationServices) : this(appApplicationServices, new TokenProviderConfiguration())
+        {
+            _appApplicationServices = appApplicationServices;
+        }
+
+        private AdditionalScopeHandler(TokenProviderConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private AdditionalScopeHandler(IServiceProvider appApplicationServices, TokenProviderConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<string> GetBearerTokenAsync()
+        {
+            try
+            {
+                var httpContext = _appApplicationServices.GetService<IHttpContextAccessor>().HttpContext;
+                var cache = httpContext.RequestServices.GetService<TokenCacheBase>();
+                var context = _configuration.ConfidentialClientApplication(cache, null);
+                var user = (await context.GetAccountsAsync()).FirstOrDefault();
+                if (user == null)
+                {
+                    throw new ServerException(new ErrorDetail
+                    {
+                        Message = "Invalid token cache"
+                    }, HttpStatusCode.Unauthorized);
+                }
+                var token = await context.AcquireTokenSilent(new[] { "https://dnvglb2cprod.onmicrosoft.com/2a9cdff4-d1fc-432d-b99b-34e7cff139df/user_impersonation" }, user).ExecuteAsync();
+                return token.CreateAuthorizationHeader();
+            }
+            catch (Exception ex)
+            {
+                _appApplicationServices.GetService<ILogger<TokenProvider>>().LogError(ex, ex.Message);
+                return null;
+            }
+        }
+
+        public void Apply(HttpRequestMessage req)
+        {
+            Task.Run(async () => await ApplyAsync(req)).GetAwaiter().GetResult();
+        }
+
+        public async Task ApplyAsync(HttpRequestMessage req)
+        {
+            var token = await GetBearerTokenAsync();
+            if (token.ContainsCharacters())
+                req.Headers.Add("Authorization", token);
+        }
+
+        public void BodyData(byte[] body)
+        {
+            
+        }
+    }
+
+    public class TestApi:ITestService
+    {
+        private Dictionary<string, string> _headers;
+        private ControllerContext _context;
+
+        public async Task<string> Get()
+        {
+            return "Yes, it works!!";
+        }
+
+        public void SetControllerContext(ControllerContext currentContext)
+        {
+            _context = currentContext;
+        }
+
+        public void SetResponseHeaderCollection(Dictionary<string, string> headers)
+        {
+            _headers = headers;
+        }
+
+        public Dictionary<string, string> GetHeaders()
+        {
+            return _headers;
+        }
+    }
+}
